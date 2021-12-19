82c814dcf8642f5a4277695e14cd19daa72b9418
Ali Aizad <aliaizad@protonmail.com>
2021-12-19 17:09:56 +0800

    Initial set-up

---
+++ script.js
@@ -0,0 +1,6 @@
+const gridCon = document.querySelector('#grid-container');
+gridCon.setAttribute('style', 'display: flex');
+//create new div
+const div = document.createElement('div');
+//append into the container
+gridCon.appendChild(div);
