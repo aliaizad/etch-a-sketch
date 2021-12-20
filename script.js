842b913ae42c4c0348fff25ad1e8d214587becf1
Ali Aizad <aliaizad@protonmail.com>
2021-12-20 14:56:03 +0800

    able to make the hovering work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gridCon = document.querySelector('#grid-container');
-gridCon.setAttribute('style', 'display: flex; height:100vh');
+gridCon.setAttribute('style', 'display: flex; height:95vh; margin: 15px 20px 0');
 //creating 16 row grid
 for (let n = 16; n > 0; n--) {
     const div = document.createElement('div');
@@ -16,7 +16,26 @@ divs.shift();
 for (const div of divs) {
     for (let n = 16; n > 0; n--) {
         const newdiv = document.createElement('div');
-        newdiv.setAttribute('style', 'border-bottom: black solid; flex: 1');
+        if (n === 1) {
+            newdiv.setAttribute('style', 'flex: 1');
+        } else {
+            newdiv.setAttribute('style', 'border-bottom: black solid; flex: 1');
+        }
         div.appendChild(newdiv); 
     }
+}
+
+//select all divs except the container;
+const hoverDivs = Array.from(document.querySelectorAll('div'));
+hoverDivs.shift();
+//removing column container divs from getting selected
+for (i = 0; i <= 256; i += 16) {
+    hoverDivs.splice(i, 1);
+}
+//setting up an event listener for the divs to change when it is hovered
+hoverDivs.forEach(square => square.addEventListener('mouseover', changeColour));
+
+//change the colour of the background if mouse is hover
+function changeColour(e) {
+    e.target.style.backgroundColor = 'black';
 }
