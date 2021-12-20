b0ea7fe8e1970de137999593ddead49fe7efd4a1
Ali Aizad <aliaizad@protonmail.com>
2021-12-20 14:03:16 +0800

    Finished creating the 16x16 grid

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const gridCon = document.querySelector('#grid-container');
-gridCon.setAttribute('style', 'display: flex');
-//create new div
-const div = document.createElement('div');
-//append into the container
-gridCon.appendChild(div);
+gridCon.setAttribute('style', 'display: flex; height:100vh');
+//creating 16 row grid
+for (let n = 16; n > 0; n--) {
+    const div = document.createElement('div');
+    if (n === 1) {
+        div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1');
+    } else {
+        div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1; border-right: 0px');
+    }
+    gridCon.appendChild(div);
+}
+//for each of the row grid, creating 16 column grid
+const divs = Array.from(document.querySelectorAll('div'));
+divs.shift();
+for (const div of divs) {
+    for (let n = 16; n > 0; n--) {
+        const newdiv = document.createElement('div');
+        newdiv.setAttribute('style', 'border-bottom: black solid; flex: 1');
+        div.appendChild(newdiv); 
+    }
+}
