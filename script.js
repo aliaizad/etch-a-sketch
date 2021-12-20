3e2d23827a0c1e4569973553656f3af30abe40a8
Ali Aizad <aliaizad@protonmail.com>
2021-12-20 17:07:15 +0800

    completed the basic website without CSS

--- script.js
+++ script.js
@@ -1,41 +1,77 @@
+const body = document.querySelector('body');
 const gridCon = document.querySelector('#grid-container');
-gridCon.setAttribute('style', 'display: flex; height:95vh; margin: 15px 20px 0');
-//creating 16 row grid
-for (let n = 16; n > 0; n--) {
-    const div = document.createElement('div');
-    if (n === 1) {
-        div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1');
-    } else {
-        div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1; border-right: 0px');
+gridCon.setAttribute('style', 'display: flex; height:80%; width:80%; justify-content: center');
+//create a function that produces n number of row grid
+function rowGrid (n) {
+    for (; n > 0; n--) {
+        const div = document.createElement('div');
+        if (n === 1) {
+            div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1');
+        } else {
+            div.setAttribute('style', 'border: black solid; display: flex; flex-direction: column; flex: 1; border-right: 0px');
+        }
+        gridCon.appendChild(div);
     }
-    gridCon.appendChild(div);
 }
-//for each of the row grid, creating 16 column grid
-const divs = Array.from(document.querySelectorAll('div'));
-divs.shift();
-for (const div of divs) {
-    for (let n = 16; n > 0; n--) {
+//create a function that creates n number of boxes in a column in the row.
+function columnGrid (n, element) {
+    for (; n > 0; n--) {
         const newdiv = document.createElement('div');
         if (n === 1) {
             newdiv.setAttribute('style', 'flex: 1');
         } else {
             newdiv.setAttribute('style', 'border-bottom: black solid; flex: 1');
         }
-        div.appendChild(newdiv); 
+        element.appendChild(newdiv); 
     }
 }
-
-//select all divs except the container;
-const hoverDivs = Array.from(document.querySelectorAll('div'));
-hoverDivs.shift();
-//removing column container divs from getting selected
-for (i = 0; i <= 256; i += 16) {
-    hoverDivs.splice(i, 1);
+//a function that remove excess container divs from being able to be hovered
+function removeExcess(n) {
+    const hoverDivs = Array.from(document.querySelectorAll('div'));
+    hoverDivs.shift();
+    //removing column container divs from getting selected
+    for (i = 0; i <= n*n; i += n) {
+        hoverDivs.splice(i, 1);
+    }
+    return hoverDivs;
 }
-//setting up an event listener for the divs to change when it is hovered
-hoverDivs.forEach(square => square.addEventListener('mouseover', changeColour));
+
+
+//a function that creates a nxn grid
+function createGrid(n) {
+    while (gridCon.firstChild) {
+        gridCon.removeChild(gridCon.lastChild);
+    }
+    rowGrid(n); //setting initial row grid
+    const divArray = Array.from(document.querySelectorAll('div'));
+    divArray.shift(); //removing the container from the array
+    divArray.forEach(div => columnGrid(n, div));
+    return removeExcess(n);
+}
+
+let gridArray = createGrid(10);//setting initial grid 
 
 //change the colour of the background if mouse is hover
 function changeColour(e) {
     e.target.style.backgroundColor = 'black';
 }
+
+//add a button before the first div
+let reset = document.createElement('button');
+reset.textContent = 'Reset';
+body.insertBefore(reset, gridCon);
+
+reset.addEventListener('click', resetAndAsk);
+
+function resetAndAsk () {
+    gridArray.forEach(div => div.style.backgroundColor = '');
+    let gridLength = +prompt('How many boxes do you want in a row (max 100)?');
+    if (!gridLength) {
+        alert("Check your input!");
+        location.reload();
+    }
+    gridArray = createGrid(gridLength);
+    gridArray.forEach(square => square.addEventListener('mouseover', changeColour));
+}
+//setting up an event listener for the divs to change when it is hovered
+gridArray.forEach(square => square.addEventListener('mouseover', changeColour));
